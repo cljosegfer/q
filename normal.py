58a38e598d575d1a7e22760fc5a95b064ad149e9
jose geraldo fernandes <macaverde604@gmail.com>
2022-08-26 16:01:11 -0300

    melhorias no metodo

--- normal.py
+++ normal.py
@@ -10,7 +10,6 @@ import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
-import matplotlib.cm as cm
 
 def lara_graph(X):
     D = distance_matrix(X, X) ** 2
@@ -26,8 +25,22 @@ def lara_graph(X):
                 adjacencia[j, i] = 1
     return adjacencia
 
+def q_index(X, y, gg):
+    scores = []
+    for i, row in enumerate(gg):
+        vizinhos = np.where(row == 1)[0]
+        
+        degree = len(vizinhos)
+        opposite = 0
+        for vizinho in vizinhos:
+            opposite += np.exp(-np.linalg.norm(X[i] - X[vizinho])) * np.abs(y[i] - y[vizinho]) / 2
+        q = 1 - opposite / degree
+        scores.append(q)
+    scores = np.array(scores)
+    return scores
+
 # data
-sd = 0.01
+sd = 0.5
 n = 100
 
 mean = (0, 0)
@@ -44,17 +57,18 @@ y = np.array([-1] * n + [1] * n)
 # gg
 gg = lara_graph(X)
 
-scores = []
-for i, row in enumerate(gg):
-    vizinhos = np.where(row == 1)[0]
+# scores = []
+# for i, row in enumerate(gg):
+#     vizinhos = np.where(row == 1)[0]
     
-    degree = len(vizinhos)
-    opposite = 0
-    for vizinho in vizinhos:
-        opposite += np.exp(-np.linalg.norm(X[i] - X[vizinho])) * np.abs(y[i] - y[vizinho]) / 2
-    q = 1 - opposite / degree
-    scores.append(q)
-scores = np.array(scores)
+#     degree = len(vizinhos)
+#     opposite = 0
+#     for vizinho in vizinhos:
+#         opposite += np.exp(-np.linalg.norm(X[i] - X[vizinho])) * np.abs(y[i] - y[vizinho]) / 2
+#     q = 1 - opposite / degree
+#     scores.append(q)
+# scores = np.array(scores)
+scores = q_index(X, y, gg)
 print(sd, np.mean(scores))
 
 # classifier
@@ -64,10 +78,12 @@ w = w / np.linalg.norm(w)
 distances = []
 for i, x in enumerate(X):
     x = np.hstack((1, x))
-    d = np.dot(x, w)
+    # d = np.dot(x, w)
+    d = np.dot(x, w) * np.sign(y[i])
     distances.append(d)
 distances = np.array(distances)
-erro = (np.sign(distances) == np.sign(y)) * 1
+# erro = (np.sign(distances) == np.sign(y)) * 1
+erro = distances < 0
 
 # plot
 plt.figure()
@@ -78,36 +94,28 @@ plt.xlim([-1, 2])
 plt.ylim([-1, 2])
 
 log = np.stack((scores, distances)).T
-colors = {0:'red', 1:'blue'}
+colors = {0:'blue', 1:'red'}
 
 plt.figure()
-plt.scatter(log[:n, 0], log[:n, 1], c = [colors[e] for e in erro[:n]])
+plt.scatter(log[:, 0], log[:, 1], c = [colors[e] for e in erro[:]])
 plt.axhline(y=0)
 plt.xlim([0, 1])
-plt.title('classe 1, sd = {}'.format(sd))
-plt.xlabel('q')
-plt.ylabel('<x, w>')
-
-plt.figure()
-plt.scatter(log[n:, 0], log[n:, 1], c = [colors[e] for e in erro[n:]])
-plt.axhline(y=0)
-plt.xlim([0, 1])
-plt.title('classe 2, sd = {}'.format(sd))
+plt.title('<x, w> vs q, sd = {}'.format(sd))
 plt.xlabel('q')
 plt.ylabel('<x, w>')
 
 plt.figure()
 y_lower = 10
-for c in np.unique(y):
-    cluster = y == c
+for c in [1, 0]:
+    cluster = erro == c
     ith_cluster_silhouette_values = scores[cluster]
     ith_cluster_silhouette_values.sort()
 
     size_cluster_i = ith_cluster_silhouette_values.shape[0]
     y_upper = y_lower + size_cluster_i
 
-    color = cm.nipy_spectral(float(c) / len(np.unique(y)))
-    # color = [colors[e] for e in erro[cluster]]
+    # color = cm.nipy_spectral(float(c) / len(np.unique(y)))
+    color = colors[c]
     plt.fill_betweenx(
         np.arange(y_lower, y_upper),
         0,
@@ -117,5 +125,5 @@ for c in np.unique(y):
         alpha=0.7,
     )
     y_lower = y_upper + 10
-plt.axvline(x=0.5, color="red", linestyle="--")
+plt.axvline(x=np.mean(scores), color="red", linestyle="--")
 plt.yticks([])
